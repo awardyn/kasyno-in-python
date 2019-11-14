5740e3188c964d507c49eaece56e0243bfabf0ec
= <=>
2019-11-14 13:03:43 +0100

    Dodanie sprawdzania checi wejscia do kasyna oraz rozpoczecie projektowania menu wyboru gier. Dodanie stanuKonta gracza

--- projektKasyna.py
+++ projektKasyna.py
@@ -1,20 +1,43 @@
+def menuGier():
+    print(stanKonta)
+
 def doWidzenia():
-    print("Do widzenia, dziękujemy za pobyt w naszym kasynie!")
+    print("Do widzenia! Do zobaczenia następnym razem!")
+def czyChceszWejsc():
+    tak = False
+    while tak == False:
+        wejscie = str(input("Czy jestes pewien, ze chcesz rzucic sie w wir szaleństwa kasynowego? [T/N] "))
+        if wejscie == "T":
+            print("Super! Zaczynajmy!")
+            tak = True
+        elif wejscie == "N":
+            print("Szkoda!")
+            doWidzenia()
+            break
+        else:
+            blad()
+    menuGier()
+        
+def blad():
+    print("Błąd wprowadzenia, powtórz, wpisując T dla tak lub N dla nie")
+
 
 def czyPelnoletni():
     pelnoletni = False
     while pelnoletni == False:
-        pelnoletnosc = str(input("Czy jesteś pełnoletni? [T/N]" ))
+        pelnoletnosc = str(input("Czy jesteś pełnoletni? [T/N] " ))
         if pelnoletnosc == "T":
             print("Super, czyli możesz wejść do kasyna!")
             pelnoletni = True
         elif pelnoletnosc == "N":
             print("Niestety musisz opuścić ten lokal")
+            doWidzenia()
             break
         else:
-            print("Błąd wprowadzenia, powtórz, wpisując T dla tak lub N dla nie")
+            blad()
+    czyChceszWejsc()
 
 
 print("Witaj w kasynie W&W")
+stanKonta = float(1000)
 czyPelnoletni()
-doWidzenia()
