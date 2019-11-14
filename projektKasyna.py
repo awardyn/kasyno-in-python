d734000f987444f3441cfcb2e4e887b4436ce013
= <=>
2019-11-14 13:17:42 +0100

    Dodanie testowej listy gier i mozliwosci wyboru jednej z nich, dodanie wypisywania stanuKonta po wyjsciu z kasyna jezli uzytkownik sie w nim wogole znalazl

--- projektKasyna.py
+++ projektKasyna.py
@@ -1,8 +1,38 @@
 def menuGier():
-    print(stanKonta)
+    czyWKasynie = True
+    print("Twoj aktualny stan konta to " + str(stanKonta) + ". W co chciałbyś aktualnie zagrać?")
+    listaGier = ['abc','bcd','cde','def','efg']
+    for i in range(len(listaGier)):
+        print("Jeżeli chcesz zagrać w " + listaGier[i] + " wybierz " + str(i+1))
+    print("Jeżeli chcesz wyjść z kasyna wybierz 0")
+    prawidlowyWybor = False
+    while prawidlowyWybor == False:
+        wybor = int(input())
+        if wybor == 1:
+            print("1")
+            prawidlowyWybor = True
+        elif wybor == 2:
+            print("2")
+            prawidlowyWybor = True
+        elif wybor == 3:
+            print("3")
+            prawidlowyWybor = True
+        elif wybor == 4:
+             print("4")
+             prawidlowyWybor = True
+        elif wybor == 5:
+            print("5")
+            prawidlowyWybor = True
+        elif wybor == 0:
+            doWidzenia(czyWKasynie)
+            prawidlowyWybor = True
+        else:
+            print("Bledny wybor sprobuj ponownie ")
 
-def doWidzenia():
+def doWidzenia(bylWKasynie):
     print("Do widzenia! Do zobaczenia następnym razem!")
+    if bylWKasynie == True:
+        print("Twoj stan konta po wyjsciu wynosi " + str(stanKonta))
 def czyChceszWejsc():
     tak = False
     while tak == False:
@@ -12,11 +42,12 @@ def czyChceszWejsc():
             tak = True
         elif wejscie == "N":
             print("Szkoda!")
-            doWidzenia()
+            doWidzenia(czyWKasynie)
             break
         else:
             blad()
-    menuGier()
+    if tak == True:
+        menuGier()
         
 def blad():
     print("Błąd wprowadzenia, powtórz, wpisując T dla tak lub N dla nie")
@@ -31,13 +62,15 @@ def czyPelnoletni():
             pelnoletni = True
         elif pelnoletnosc == "N":
             print("Niestety musisz opuścić ten lokal")
-            doWidzenia()
+            doWidzenia(czyWKasynie)
             break
         else:
             blad()
-    czyChceszWejsc()
+    if pelnoletni == True:
+        czyChceszWejsc()
 
 
 print("Witaj w kasynie W&W")
 stanKonta = float(1000)
+czyWKasynie = False
 czyPelnoletni()
