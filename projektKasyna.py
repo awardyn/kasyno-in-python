051661c8a4593dd2b0880e41e08129a8548ff1e9
= <=>
2019-11-14 12:47:21 +0100

    Dodanie przywitania sie, sprawdzenie czy jestes pelnoletni oraz pozegnanie sie

--- projektKasyna.py
+++ projektKasyna.py
@@ -1,8 +1,20 @@
+def doWidzenia():
+    print("Do widzenia, dziękujemy za pobyt w naszym kasynie!")
+
+def czyPelnoletni():
+    pelnoletni = False
+    while pelnoletni == False:
+        pelnoletnosc = str(input("Czy jesteś pełnoletni? [T/N]" ))
+        if pelnoletnosc == "T":
+            print("Super, czyli możesz wejść do kasyna!")
+            pelnoletni = True
+        elif pelnoletnosc == "N":
+            print("Niestety musisz opuścić ten lokal")
+            break
+        else:
+            print("Błąd wprowadzenia, powtórz, wpisując T dla tak lub N dla nie")
+
+
 print("Witaj w kasynie W&W")
-pelnoletnosc = str(input("Czy jesteś pełnoletni? [T/N]" ))
-if pelnoletnosc == "T":
-    print("Super")
-elif pelnoletnosc == "N":
-    print("Niestety musisz opuścić ten lokal")
-else:
-    print("Powtórz, wpisując T dla tak lub N dla nie")
+czyPelnoletni()
+doWidzenia()
