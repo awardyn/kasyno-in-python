b26c85e3d56d053c18af9ccb8dc1359867c91e6f
= <=>
2019-11-15 09:04:45 +0100

    Dodanie wypisywania kart dealera i kart gracza na poczatku, dodanie funkcji sumowania punktacji kart dealera i gracza

--- projektKasyna.py
+++ projektKasyna.py
@@ -1,14 +1,80 @@
 import random
+def punkty(wartosc):
+    punktyZdobyte = int(0)
+    if wartosc == 'Dwa':
+            punktyZdobyte = 2
+    elif wartosc == 'Trzy':
+            punktyZdobyte = 3
+    elif wartosc == 'Cztery':
+            punktyZdobyte = 4
+    elif wartosc == 'Pięć':
+            punktyZdobyte = 5
+    elif wartosc == 'Sześć':
+            punktyZdobyte = 6
+    elif wartosc == 'Siedem':
+            punktyZdobyte = 7
+    elif wartosc == 'Osiem':
+            punktyZdobyte = 8
+    elif wartosc == 'Dziewięć':
+            punktyZdobyte = 9
+    elif wartosc == 'Walet':
+            punktyZdobyte = 10
+    elif wartosc == 'Dama':
+            punktyZdobyte = 10
+    elif wartosc == 'Król':
+            punktyZdobyte = 10
+    elif wartosc == 'As':
+            punktyZdobyte = 1
+    return punktyZdobyte
+def punktacjaKart(kartyGracza, kartyDealera):
+    punktyGracza = int(0)
+    punktyDealera = int(0)
+    graczMaAsa = False
+    dealerMaAsa = False
+    for i in range(len(kartyGracza)):
+        wartosc = kartyGracza[i][1]
+        punktyKarty = punkty(wartosc)
+        if wartosc == 'As':
+            graczMaAsa = True
+        punktyGracza += punktyKarty
+    if (graczMaAsa == True and punktyGracza + 10<=21):
+        punktyGracza +=10
+    for i in range(len(kartyDealera)):
+        wartosc = kartyDealera[i][1]
+        punktyKarty = punkty(wartosc)
+        if wartosc == 'As':
+            dealerMaAsa = True
+        punktyDealera += punktyKarty
+    return punktyGracza, punktyDealera
+    
+        
+
 def rundaBlackjack(karty):
     kartyGracza = []
     kartyDealera = []
-    while len(kartyGracza)<2:
+    while len(kartyGracza) < 2:
         nazwa, slownikWartosci = random.choice(list(karty.items()))
         wartosc, czyWybrana = random.choice(list(slownikWartosci.items()))
         if czyWybrana == False:
             kartyGracza.append([nazwa,wartosc])
             karty[nazwa][wartosc] = True
-    print(kartyGracza)
+    while len(kartyDealera) < 2:
+        nazwa, slownikWartosci = random.choice(list(karty.items()))
+        wartosc, czyWybrana = random.choice(list(slownikWartosci.items()))
+        if czyWybrana == False:
+            kartyDealera.append([nazwa,wartosc])
+            karty[nazwa][wartosc] = True
+    punktacjaGracza, punktacjaDealera  = punktacjaKart(kartyGracza,kartyDealera)
+    print("Karty dealera to:")
+    for i in range(len(kartyDealera)):
+        print(kartyDealera[i][1] + " " + kartyDealera[i][0])
+    print("(Punktacja kart: " + str(punktacjaDealera) + ")")
+    print(" ")
+    print("Karty gracza to:")
+    for i in range(len(kartyGracza)):
+        print(kartyGracza[i][1] + " " + kartyGracza[i][0])
+    print("(Punktacja kart: " + str(punktacjaGracza) + ")")
+
 
 def blackjackWprowadzenie():
     karty = {'Serce': {'Dwa': False, 'Trzy': False, 'Cztery': False, 'Pięć': False, 'Sześć': False, 'Siedem': False, 'Osiem': False, 'Dziewięć': False, 'Dziesięć': False, 'Walet': False, 'Dama': False, 'Król': False, 'As': False}, 'Piki':{'Dwa': False, 'Trzy': False, 'Cztery': False, 'Piec': False, 'Szesc': False, 'Siedem': False, 'Osiem': False, 'Dziewiec': False, 'Dziesiec': False, 'Walet': False, 'Dama': False, 'Król': False, 'As': False}, 'Trefle':{'Dwa': False, 'Trzy': False, 'Cztery': False, 'Piec': False, 'Szesc': False, 'Siedem': False, 'Osiem': False, 'Dziewiec': False, 'Dziesiec': False, 'Walet': False, 'Dama': False, 'Król': False, 'As': False},'Kiery':{'Dwa': False, 'Trzy': False, 'Cztery': False, 'Piec': False, 'Szesc': False, 'Siedem': False, 'Osiem': False, 'Dziewiec': False, 'Dziesiec': False, 'Walet': False, 'Dama': False, 'Król': False, 'As': False}}
