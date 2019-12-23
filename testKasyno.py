1b18da3f7c9650f9bb8d30e5298a851f315955c3
MelkorW <olekwardyn@gmail.com>
2019-12-23 15:21:18 +0100

    Dodanie testow do automatu

--- testKasyno.py
+++ testKasyno.py
@@ -1,7 +1,7 @@
 import wspolneFunkcje
 import koloRuletka
 import blackjack
-
+import automat
 
 def testkwotaDoObstawienia():
     assert type([]) == type(wspolneFunkcje.dobieranieKarty({'Serce': {'Dwa': False, 'Trzy': False, 'Cztery': False,
@@ -44,3 +44,8 @@ def testkrecenieKolem():
 
 def testdobieranieKarty():
     assert wspolneFunkcje.dobieranieKarty({'Kiery': {'Dwa': False}}, []) == [['Kiery', 'Dwa']]
+
+
+def testwygrana():
+    assert automat.wygrana({"arbuz": 1, "pomarancz": 1, "truskawka": 1, "siodemka": 2, "bonus": "bonus", "winogrona": 1, "malina": 1, "dziesiatka": 2, "czeresnia": 1}, ['dodatkowy rzut', 'wygrana 10x', 'dodatkowy rzut', 'dodatkowy rzut', 'dodatkowy rzut', 'wygrana 3x', 'wygrana 3x'], ["arbuz", "pomarancz", "truskawka", "siodemka", "bonus", "winogrona", "malina", "dziesiatka", "czeresnia"], 0, 0, False) == (1, False)
+    assert automat.wygrana({"arbuz": 1, "pomarancz": 1, "truskawka": 1, "siodemka": 2, "bonus": "bonus", "winogrona": 1, "malina": 1, "dziesiatka": 2, "czeresnia": 1}, ['dodatkowy rzut', 'dodatkowy rzut', 'dodatkowy rzut', 'dodatkowy rzut'], ["arbuz", "pomarancz", "truskawka", "siodemka", "bonus", "winogrona", "malina", "dziesiatka", "czeresnia"], 4, 10, False) == (10, True)
