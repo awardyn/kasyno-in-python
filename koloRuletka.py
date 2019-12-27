343d8458b96389d719c3fa158606121ed9d7b503
MelkorW <olekwardyn@gmail.com>
2019-12-27 16:15:48 +0100

    Dodanie zagniezdzonej tablicy tuzinow do ruletki

--- koloRuletka.py
+++ koloRuletka.py
@@ -1,22 +1,28 @@
 import random
 import time
 
+
 def sprawdzeniewyniku(wynik, ruletka):
-    if wynik %2 == 0:
+    tuziny = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12], [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
+              [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]]
+    if wynik % 2 == 0:
         parzystosc = 'parzyste'
     else:
         parzystosc = 'nieparzyste'
     if wynik in ruletka['Czerwone']:
         kolor = 'czerwone'
-    else:
+    elif wynik in ruletka['Czarne']:
         kolor = 'czarne'
-    if wynik//12 == 0:
+    else:
+        kolor = 'zielone'
+    if wynik in tuziny[0]:
         tuzin = 'pierwszy tuzin'
-    elif wynik//12 == 1:
+    elif wynik in tuziny[1]:
         tuzin = 'drugi tuzin'
     else:
         tuzin = 'trzeci tuzin'
     return parzystosc, kolor, tuzin
+
 
 def krecenieKolem():
     zmienna = 3
